f44e692c8a0ae522798de5ff9ea572151db44545
Sergio Forja Barriga <sergio.forja88@gmail.com>
2024-03-26 17:59:50 +0100

    Cambios en JS

--- src/assets/scripts/poblacion.js
+++ src/assets/scripts/poblacion.js
@@ -1,4 +1,4 @@
-import Chart from 'chart.js/auto'
+/* import Chart from 'chart.js/auto'
 
 (async function() {
   const data = [
@@ -35,4 +35,4 @@ import Chart from 'chart.js/auto'
       }
     }
   );
-})();
+})(); */
